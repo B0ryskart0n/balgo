b972969c0096360f7baebfb596449e1848009170
Borys Kopeć <boryskopec00@gmail.com>
2025-06-11 17:20:20 +0200

    Change crate type to library

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,6 @@ mod tests;
 
 use std::collections::HashMap;
 
-fn main() {}
-
 pub fn a_star(graph: &HashMap<Id, Vec<(Id, Weight)>>, start: Id, goal: Id) -> (Vec<Id>, Weight) {
     let mut path: Vec<Id> = Vec::new();
     let mut weight = 0;
